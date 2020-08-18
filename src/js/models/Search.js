c7bf832c23ea51a7d78ab62def67051dfac4550c
sanderdebr <sander606@gmail.com>
2020-08-18 17:02:37 +0200

    tut 1

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -9,7 +9,7 @@ export default class Search {
   async getPictures() {
     try {
       const result = await axios(
-        `http://www.splashbase.co/api/v1/images/search?query=${this.query}`
+        `https://www.splashbase.co/api/v1/images/search?query=${this.query}`
       );
       this.pictures = result.data.images;
     } catch (error) {
